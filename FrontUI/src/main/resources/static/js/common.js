10f03ccd0eaf1049ae514c05feeb48731547972d
Hyeopgeon Lee <39357722+Hyeopgeon-Lee@users.noreply.github.com>
2023-11-11 12:25:42 +0900

    AppManager 추가

--- FrontUI/src/main/resources/static/js/common.js
+++ FrontUI/src/main/resources/static/js/common.js
@@ -1,3 +1,14 @@
 // API 서버 정보 기입
 const apiServer = "localhost:13000";
 const loginPage = "/ss/login.html";
+const jwtTokenName = "jwtAccessToken";
+
+function loginCheck() {
+    let token = $.cookie(jwtTokenName);
+
+    if (token == "undefined" || token == null) {
+        alert("로그인이 되지 않았습니다. 로그인하길 바랍니다.");
+        location.href = loginPage;
+
+    }
+}
