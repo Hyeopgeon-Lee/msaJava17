34e92e8dcb4f57c497817b50c9f061af26135045
Hyeopgeon Lee <39357722+Hyeopgeon-Lee@users.noreply.github.com>
2023-11-13 18:56:57 +0900

    Spring Config 변경

--- FrontUI/src/main/resources/static/js/common.js
+++ FrontUI/src/main/resources/static/js/common.js
@@ -12,3 +12,21 @@ function loginCheck() {
 
     }
 }
+
+//글자 길이 바이트 단위로 체크하기(바이트값 전달)
+function calBytes(str) {
+    let tcount = 0;
+    let tmpStr = String(str);
+    let strCnt = tmpStr.length;
+    let onechar;
+    for (let i = 0; i < strCnt; i++) {
+        onechar = tmpStr.charAt(i);
+        if (escape(onechar).length > 4) {
+            tcount += 2;
+        } else {
+            tcount += 1;
+        }
+    }
+    return tcount;
+}
+
