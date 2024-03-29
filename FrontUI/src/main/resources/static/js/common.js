21b69cb3a6eddf58559be94a69b97c04a046808a
Hyeopgeon Lee <soft1318@naver.com>
2024-03-29 17:21:34 +0900

    UI 수정

--- FrontUI/src/main/resources/static/js/common.js
+++ FrontUI/src/main/resources/static/js/common.js
@@ -1,14 +1,17 @@
 // API 서버 정보 기입
-const apiServer = "localhost:13000";
+const apiServer = "localhost:11000";
 const loginPage = "/ss/login.html";
 const jwtTokenName = "jwtAccessToken";
 
 function loginCheck() {
     let token = $.cookie(jwtTokenName);
 
+    alert("token : " + $.cookie());
+    alert("token : " + $.cookie("jwtAccessToken"));
+    alert("token : " + $.cookie("jwtAccessToken"));
     if (token === undefined || token == null) {
         alert("로그인이 되지 않았습니다. 로그인하길 바랍니다.");
-        location.href = loginPage;
+        // location.href = loginPage;
 
     }
 }
